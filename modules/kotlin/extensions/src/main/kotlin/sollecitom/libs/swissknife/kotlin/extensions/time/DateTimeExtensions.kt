d0854636ec4ba3213bce304ab30b788ec91dd1f4
michele <michele.sollecito@gmail.com>
2025-07-06 19:38:54 +0100

    New module structure

--- modules/kotlin/extensions/src/main/kotlin/sollecitom/libs/swissknife/kotlin/extensions/time/DateTimeExtensions.kt
+++ modules/kotlin/extensions/src/main/kotlin/sollecitom/libs/swissknife/kotlin/extensions/time/DateTimeExtensions.kt
@@ -1,9 +1,13 @@
 package sollecitom.libs.swissknife.kotlin.extensions.time
 
 import kotlinx.datetime.DatePeriod
+import kotlinx.datetime.YearMonth
+import kotlinx.datetime.toJavaMonth
+import java.time.format.TextStyle
+import java.time.temporal.ChronoUnit
+import java.util.*
 import kotlin.time.Instant
 import kotlin.time.toJavaInstant
-import java.time.temporal.ChronoUnit
 import kotlin.time.toKotlinInstant
 
 fun Instant.truncatedToMilliseconds(): Instant = toJavaInstant().truncatedTo(ChronoUnit.MILLIS).toKotlinInstant()
@@ -13,3 +17,5 @@ fun Instant.truncatedToSeconds(): Instant = toJavaInstant().truncatedTo(ChronoUn
 val Int.years: DatePeriod get() = DatePeriod(years = this)
 
 val Int.months: DatePeriod get() = DatePeriod(months = this)
+
+fun YearMonth.displayName(style: TextStyle = TextStyle.SHORT, locale: Locale = Locale.getDefault()) = "${month.toJavaMonth().getDisplayName(style, locale)} $year"
