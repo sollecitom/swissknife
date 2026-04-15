2ed9efa75bc90bcf7ae80f38c7f4aa0b7f0d39c0
Michele Sollecito <msollecito@meta.com>
2026-04-15 13:29:54 +0100

    WIP

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     alias(libs.plugins.com.palantir.git.version)
-    id("sollecitom.dependency-update-conventions")
-    id("sollecitom.aggregate-test-metrics-conventions")
+    alias(libs.plugins.sollecitom.dependency.update.conventions)
+    alias(libs.plugins.sollecitom.aggregate.test.metrics.conventions)
+    alias(libs.plugins.sollecitom.kotlin.library.conventions) apply false
+    alias(libs.plugins.sollecitom.maven.publish.conventions) apply false
 }
